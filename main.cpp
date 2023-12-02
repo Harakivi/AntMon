f94ec918d8c5d0b376f28f2d969896ce0244bc71
Evgenii Kotyshkov <Harakivi@gmail.com>
2023-12-02 17:19:35 +0700

    indicator soc_test

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@
 #include "dac.hpp"
 #include "cfifo.h"
 
-enum CliStates
+enum TestModes
 {
-    Clear = 0,
-    SOC,
+    None = 0,
     DacTestMode,
+    SOCTestMode,
 };
 
 extern "C" void SystemClock_Config();
+
+static const uint16_t dacTable[] = {2400, 2750, 2925, 3075, 3350};
 
 typedef InternalPeriph::Uart<1> cliUart;
 typedef InternalPeriph::Uart<2> bmsUart;
@@ -23,7 +25,7 @@ Dac *dac = Dac::Get();
 typedef Drivers::Cli Cli;
 #ifdef DEBUG
 Cli cli = Cli(cliUart::Get());
-CliStates cliState;
+TestModes testMode = None;
 #endif
 
 bool valid = true;
@@ -32,6 +34,8 @@ uint16_t dacValCh1 = 0;
 uint32_t liveDataReadLastTime = 0;
 
 #ifdef DEBUG
+uint8_t test_SOC = 0;
+
 void printSOC()
 {
     cli.clear();
@@ -52,7 +56,6 @@ void printSOC()
 
 void stateSOC()
 {
-    cliState = SOC;
     cli.setHeaderUpdater(printSOC);
 }
 
@@ -61,24 +64,36 @@ void dacSetVal(uint16_t value)
     dac->SetValue(value, 1);
 }
 
+void socSetVal(uint16_t value)
+{
+    test_SOC = value;
+}
+
 void printDAC()
 {
     cli.clear();
     cli.clearHeader();
     cli.printHeader("========================\r\n");
     cli.printHeader("DAC ch1 Value = %d\r\n", dac->GetValue(1));
+    cli.printHeader("test_SOC = %d\r\n", test_SOC);
     cli.printHeader("========================\r\n");
 }
 
 void stateDacTestMode()
 {
-    cliState = DacTestMode;
+    testMode = DacTestMode;
+    cli.setHeaderUpdater(printDAC);
+}
+
+void stateSOCTestMode()
+{
+    testMode = SOCTestMode;
     cli.setHeaderUpdater(printDAC);
 }
 
 void clearHeader()
 {
-    cliState = Clear;
+    testMode = None;
     cli.setHeaderUpdater(nullptr);
     cli.clearHeader();
     cli.clear();
@@ -96,7 +111,9 @@ int main()
     cli.Open(115200);
     cli.AddCmd(cmd_t{"print_soc", NULL, (void *)stateSOC, NULL});
     cli.AddCmd(cmd_t{"dac_test", NULL, (void *)stateDacTestMode, NULL});
+    cli.AddCmd(cmd_t{"soc_test", NULL, (void *)stateSOCTestMode, NULL});
     cli.AddCmd(cmd_t{"dac_val", NULL, (void *)dacSetVal, UINT16});
+    cli.AddCmd(cmd_t{"soc_val", NULL, (void *)socSetVal, UINT16});
     cli.AddCmd(cmd_t{"clear", NULL, (void *)clearHeader, NULL});
     cli.AddCmd(cmd_t{"reset", NULL, (void *)Reset_Handler, NULL});
 #endif
@@ -105,11 +122,26 @@ int main()
         if (!liveDataReadLastTime || (HAL_GetTick() - liveDataReadLastTime) > 2000)
         {
             livedata = ant.ReadLiveData(valid);
-            if (valid)
-            {
-                dacValCh1 = livedata.Struct.SOC * 12 + 2895;
 #ifdef DEBUG
-                if (cliState != DacTestMode)
+            if(testMode != None)
+                livedata.Struct.SOC = test_SOC;
+            if (valid || testMode == SOCTestMode)
+#else
+            if (valid)
+#endif
+            {
+                if (livedata.Struct.SOC >= 5 && livedata.Struct.SOC < 25)
+                    dacValCh1 = dacTable[1];
+                else if (livedata.Struct.SOC >= 25 && livedata.Struct.SOC < 50)
+                    dacValCh1 = dacTable[2];
+                else if (livedata.Struct.SOC >= 50 && livedata.Struct.SOC < 75)
+                    dacValCh1 = dacTable[3];
+                else if (livedata.Struct.SOC >= 75 && livedata.Struct.SOC <= 100)
+                    dacValCh1 = dacTable[4];
+                else
+                    dacValCh1 = dacTable[0];
+#ifdef DEBUG
+                if (testMode != DacTestMode)
 #endif
                     dac->SetValue(dacValCh1, 1);
             }
